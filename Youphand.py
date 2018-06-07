078fb55a138a7620a30c8099f0d17f251f3cfef1
Samantha Goh <samanthagoh@Samanthas-MacBook-Air.local>
2018-06-07 16:31:27 +0800

    First Commit

---
+++ Youphand.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = requests.get('https://www.youtube.com/feed/trending')
+soup = BeautifulSoup(url.text, 'html.parser')
+video_list=[]
+
+x = soup.find_all("a", class_="yt-uix-tile-link yt-ui-ellipsis yt-ui-ellipsis-2 yt-uix-sessionlink spf-link ")
+# print x
+for each_x in x:
+
+		print(each_x.prettify())
